de27f9de389c0554ed85a1674c6f51f43bccd466
chenxiaokunkun <1729803981@qq.com>
2018-06-19 19:26:01 +0800

    oldAndNew

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(function(){
+
+    //活动规则 事件
+    $('.huodong').click(function(){
+        $('.activity_dic').fadeIn();/*活动规则 淡入*/
+    });
+
+    //立即邀请 事件
+    $('.anniu').click(function(){
+        $('#de_fa').fadeIn();/*登陆后发送邀请 淡入*/
+        $('#shouye').fadeOut();/*主页1 淡出*/
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ js/loading.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    //loading 进度条
+    $('.gundongtiao>span').animate({width:'100%'});2000;
+
+    //window.location('');
+
+
+});
